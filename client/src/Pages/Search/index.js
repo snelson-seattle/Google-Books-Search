14e7df37c5ee02afa1f8f3277ad98282738bd746
Scott Nelson <snelson.seattle@gmail.com>
2021-02-23 00:14:46 -0800

    google books search and card printing works, but needs refinement

--- client/src/Pages/Search/index.js
+++ client/src/Pages/Search/index.js
@@ -6,10 +6,12 @@ function Search() {
     const [bookList, setBookList] = useState([]);
 
 
-    const handleSubmit = async (event) => {
+    const handleSubmit = (event) => {
         event.preventDefault();
-        const results = await API.getGoogleBook(bookTitle);
-        setBookList(results);
+        API.getGoogleBook(bookTitle).then(results => {
+            setBookList(results.items); 
+            console.log(results.items);
+        });
     };
 
 
@@ -26,8 +28,35 @@ function Search() {
                     <button className="btn btn-danger" type="submit" onClick={handleSubmit}>Search</button>
                 </form>            
             </section>
-            <section>
-                
+            <br />
+            <section className="container">
+                {bookList.map(book => {
+                    return (
+                        <div className="card mb-3" key={book.id}>
+                            <div className="row g-0">
+                                <div className="col-md-5">
+                                    <h2 className="card-title">{book.volumeInfo.title}</h2>
+                                    <h4>{book.volumeInfo.subtitle}</h4>
+                                    <p>Written by: </p>
+                                </div>
+                                <div className="col-md-7">
+                                    <button className="btn btn-primary float-end">Save</button>
+                                    <button className="btn btn-secondary float-end">View</button>
+                                </div>
+                            </div>
+                            <div className="row g-0">
+                                <div className="col-md-4">
+                                    <img src={book.volumeInfo.imageLinks.thumbnail} alt="Book Cover Image" />
+                                </div>
+                                <div className="col-md-8">
+                                    <div className="card-body">
+                                        <p className="card-text">{book.volumeInfo.description}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                })}
             </section>
         </React.Fragment>
     )
