19ed11e8987e62e2509fba5dd781a0f57b66e5fe
Scott Nelson <snelson.seattle@gmail.com>
2021-02-27 07:38:55 -0800

    deploying

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,19 +3,19 @@ const axios = require("axios");
 export default {
     // Get all books
     getBooks: () => {
-        return axios.get("/api/books/");
+        return axios.get("https://fathomless-escarpment-35808.herokuapp.com/api/books/");
     },
     getBookById: (id) => {
-        return axios.get("/api/books/" + id);
+        return axios.get("https://fathomless-escarpment-35808.herokuapp.com/api/books/" + id);
     },
     deleteBook: (id) => {
-        return axios.delete("/api/books/" + id);
+        return axios.delete("https://fathomless-escarpment-35808.herokuapp.com/api/books/" + id);
     },
     editBook: (id) => {
-        return axios.put("/api/books/" + id);
+        return axios.put("https://fathomless-escarpment-35808.herokuapp.com/api/books/" + id);
     },
     saveBook: (bookData) => {
-        return axios.post("api/books/", bookData);
+        return axios.post("https://fathomless-escarpment-35808.herokuapp.com/api/books/", bookData);
     },
     getGoogleBook: async (bookTitle) => {
         let response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookTitle}&maxResults=40`);
